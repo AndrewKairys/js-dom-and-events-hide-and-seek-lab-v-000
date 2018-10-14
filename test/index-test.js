85655f34bd860a706089f1bd413a9e91bce575d9
Andrew Kairys <andrewkairys@gmail.com>
2018-10-14 18:20:45 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 describe('index', () => {
   describe('getFirstSelector(selector)', () => {
-    it;('returns the first element that matches the selector', () => {
+    it('returns the first element that matches the selector', () => {
       expect(getFirstSelector('div').id).toEqual('nested')
       expect(getFirstSelector('.ranked-list')).toEqual(document.querySelector('.ranked-list'))
     })
